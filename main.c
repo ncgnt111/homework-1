818071fe44306159964735089ad2c78c2fd4587b
ncgnt111 <32277953+ncgnt111@users.noreply.github.com>
2017-09-27 01:07:49 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,16 +6,22 @@
 
 int main (int argc, char *argv[]) {
     if (argc != 2) {
-        printf(" The command %s must have only one argument\n", argv[0]);
+        printf(" The program %s must have only one argument!\n", argv[0]);
         return EXIT_FAILURE;
     }
 
     if (strlen(argv[1]) > PATH_MAX) {
-        printf("Length of an argument is more than PATH_MAX.\n");
+        printf("Length of an argument is more than PATH_MAX!\n");
         return EXIT_FAILURE;
     }
 
-    modify_file(argv[1]);
+    int result_modify = 0;
 
-    return EXIT_SUCCESS;
+    result_modify = modify_file(argv[1]);
+
+    if (!result_modify) {
+        return EXIT_SUCCESS;
+    } else {
+        return EXIT_FAILURE;
+    }
 }
