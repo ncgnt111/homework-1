61131f6b2ead3e452f33c9789d2a76d66835f272
ncgnt111 <32277953+ncgnt111@users.noreply.github.com>
2017-09-27 17:56:55 +0300

    Update digits.c

--- digits.c
+++ digits.c
@@ -3,7 +3,7 @@
 #include <errno.h>
 #include <string.h>
 
-int add_number_to_array(int **array, int *size_array, int number) {
+int add_number_to_array(int **array, int *used,int *size, int number) {
     int idx = 0;
 
     if (number == 0) {
@@ -14,31 +14,37 @@ int add_number_to_array(int **array, int *size_array, int number) {
         idx = 2;
     }
 
-    int *addr = NULL;
-    ++(size_array[idx]);
+    ++(used[idx]);
 
-    addr = (int*)realloc(array[idx], (size_array[idx]) * sizeof(int)); // change the size of the memory block
+    if (used[idx] > size[idx]) {
 
-    if (addr != NULL) {
-        array[idx] = addr;
-        array[idx][size_array[idx]-1] = number; // write a number to the corresponding array
-        return EXIT_SUCCESS;
-    } else {
-        printf("ERROR: %s\n", strerror(errno));
+        int *addr = NULL;
 
-        for(int i = 0; i < 3; i++)
-            free(array[i]); // realloc doesn't free it in case of fail
-        free(array);
+        size[idx] *= 2;
 
-        return EXIT_FAILURE;
+        addr = (int*)realloc(array[idx], (size[idx]) * sizeof(int)); // change the size of the memory block
+        if (addr != NULL) {
+            array[idx] = addr;
+        } else {
+            printf("ERROR: %s\n", strerror(errno));
+
+            for(int i = 0; i < 3; i++)
+                free(array[i]); // realloc doesn't free it in case of fail
+            free(array);
+
+            return EXIT_FAILURE;
+        }
     }
+
+    array[idx][used[idx] - 1] = number; // write a number to the corresponding array
+    return EXIT_SUCCESS;
 }
 
-int write_array_to_file(FILE *fp, int **array, int *size_array) {
+int write_array_to_file(FILE *fp, int **array, int *used) {
     int result_fprintf = 0;
 
     for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < size_array[i]; ++j) {
+        for (int j = 0; j < used[i]; ++j) {
 
             result_fprintf = fprintf(fp, "%d ", array[i][j]);
 
@@ -66,6 +72,10 @@ int modify_file(char *file_name) {
     }
 
     // Create multidimensional dynamic array for zeros, negative and positive values
+
+    int used[3] = {0, 0, 0};
+    int size[3] = {100, 100, 100};
+
     int **values = NULL;
     values = malloc(3 * sizeof(int *));
     if(values == NULL) {
@@ -74,10 +84,12 @@ int modify_file(char *file_name) {
     }
 
     for(int i = 0; i < 3; i++) {
-        values[i] = NULL;
+        values[i] = malloc(size[i] * sizeof(int));
+        if (values[i] == NULL) {
+            printf("ERROR: %s\n", strerror(errno));
+            return EXIT_FAILURE;
+        }
     }
-
-    int size_values[3] = {0};
 
     int number = 0;
 
@@ -94,7 +106,7 @@ int modify_file(char *file_name) {
             fclose(fp);
             return EXIT_FAILURE;
         } else {
-            result_add_number = add_number_to_array(values, size_values, number);
+            result_add_number = add_number_to_array(values, used, size, number);
             if (result_add_number == EXIT_FAILURE) {
                 fclose(fp);
                 return EXIT_FAILURE;
@@ -114,7 +126,7 @@ int modify_file(char *file_name) {
 
     // Write values from multidimensional dynamic array to the txt-file:
     // zeros, then negative and positive values
-    result_write_array = write_array_to_file(fp, values, size_values);
+    result_write_array = write_array_to_file(fp, values, used);
     if (result_write_array == EXIT_FAILURE) {
         return EXIT_FAILURE;
     }
